99eedf0a0e679b15e129a63f5654fd0b2ff29dc2
Acuina <laurie.fernandes3@epsi.fr>
2021-03-26 19:03:47 +0100

    début d'ajout du servicetournois

---
+++ Controle Rattrapage/Models/tournois.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controle_Rattrapage.Models
+{
+    public class tournois
+    {
+        public string nom;
+        public Joueursdetennis vainqueurdutournois;
+
+    }
+}
---
+++ Controle Rattrapage/Services/ServicesTournois.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Controle_Rattrapage.Models;
+
+namespace Controle_Rattrapage.Services
+{
+    public class ServicesTournois
+    {
+        //declarations des variables
+        private DemandeUsers _demandeUsers; 
+        private ServicesJoueurs _servicesJoueurs;
+        private List<tournois> Listedestournois = new List<tournois>();
+
+        //injection de dépendances
+
+        public ServicesTournois(DemandeUsers demandeUsers, ServicesJoueurs servicesJoueurs)
+        {
+            _demandeUsers = demandeUsers;
+            _servicesJoueurs = servicesJoueurs;
+        }
+
+        public void Afficheuntournois()
+        {
+            foreach (tournois t in Listedestournois)
+            {
+                Console.WriteLine("Nom du tournoi :" + t.nom);
+                Console.WriteLine("Vainqueur du tournoi :" + t.vainqueurdutournois.nom + " " + t.vainqueurdutournois.prénom);
+            }
+        }
+
+    }
+}
