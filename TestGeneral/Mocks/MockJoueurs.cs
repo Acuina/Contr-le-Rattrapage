dacf1ea89e87cf0af15d9650c383e9c635736592
Acuina <laurie.fernandes3@epsi.fr>
2021-03-27 16:11:01 +0100

    essaie bref d'un mock '-'

---
+++ TestGeneral/Mocks/MockJoueurs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Controle_Rattrapage.Models;
+using Controle_Rattrapage.Services;
+
+namespace TestGeneral.Mocks
+{
+    class MockJoueurs
+    {
+        private List<Joueursdetennis> _mockjoueursdetennis;
+
+        public override List<Joueursdetennis> getAll()
+        {
+            if (_mockjoueursdetennis == null)
+            {
+                _mockjoueursdetennis = new List<Joueursdetennis>();
+                _mockjoueursdetennis.Add(new Joueursdetennis() { nom = "Smart", prénom = "iona", classements = 5 });
+            }
+            return _mockjoueursdetennis;
+        }
+    }
+}
---
+++ TestGeneral/ServicesJoueursTestsUnitaires.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Controle_Rattrapage.Models;
+using Controle_Rattrapage.Services;
+
+namespace TestGeneral
+{
+    public class Tests
+    {
+        ServicesJoueurs js;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
